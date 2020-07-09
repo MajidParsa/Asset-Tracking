015c56892fac255905e74e6289eb27c215a6f1ae
Majid Parsa <MajidKahaki@gmail.com>
2020-07-09 14:15:00 +0430

    Add SeedWork classes

---
+++ AssetTracking.Domain/SeedWork/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetTracking.Domain.SeedWork
+{
+	public abstract class Entity
+	{
+	}
+}
---
+++ AssetTracking.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetTracking.Domain.SeedWork
+{
+	public interface IRepository<T> where T : IAggregateRoot
+	{
+	}
+}
