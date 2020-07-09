3014dfc9e3ba339d40ac9cfbeb79e2bd8c2a5110
Majid Parsa <MajidKahaki@gmail.com>
2020-07-09 14:41:05 +0430

    Create AssetGroup entity

--- AssetTracking.Domain/AggregatesModel/AssetAggregate/Asset.cs
+++ AssetTracking.Domain/AggregatesModel/AssetAggregate/Asset.cs
@@ -7,5 +7,24 @@ namespace AssetTracking.Domain.AggregatesModel.AssetAggregate
 {
 	public class Asset : Entity, IAggregateRoot
 	{
+		public int Id { get; private set; }
+		public int AssetGroupId { get; private set; }
+		public string SerialNo { get; private set; }
+		public string Title { get; private set; }
+
+
+		private Asset(int id, int assetGroupId, string serialNo, string title)
+		{
+			Id = id;
+			AssetGroupId = assetGroupId;
+			SerialNo = serialNo;
+			Title = title;
+		}
+
+		public Asset CreateAsset(int id, int assetGroupId, string serialNo, string title)
+		{
+			return new Asset(id, assetGroupId, serialNo, title);
+		}
+
 	}
 }
---
+++ AssetTracking.Domain/AggregatesModel/AssetAggregate/AssetGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssetTracking.Domain.SeedWork;
+
+namespace AssetTracking.Domain.AggregatesModel.AssetAggregate
+{
+	public class AssetGroup : Entity
+	{
+		public int Id { get; private set; }
+		public string Code { get; private set; }
+		public string Title { get; private set; }
+
+		private AssetGroup(int id, string code, string title)
+		{
+			Id = id;
+			Code = code;
+			Title = title;
+		}
+
+		public AssetGroup CreateAssetGroup(int id, string code, string title)
+		{
+			return new AssetGroup(id, code, title);
+		}
+
+	}
+}
