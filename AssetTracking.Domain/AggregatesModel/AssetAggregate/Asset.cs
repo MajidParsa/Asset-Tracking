f67c44fa8fe6e01a6e2837c1457b7d75ffbb7dbd
Majid Parsa <MajidKahaki@gmail.com>
2020-07-09 14:15:22 +0430

    Add AggregateModel

---
+++ AssetTracking.Domain/AggregatesModel/AssetAggregate/Asset.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssetTracking.Domain.SeedWork;
+
+namespace AssetTracking.Domain.AggregatesModel.AssetAggregate
+{
+	public class Asset : Entity, IAggregateRoot
+	{
+	}
+}
---
+++ AssetTracking.Domain/AggregatesModel/AssetAggregate/IAssetRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AssetTracking.Domain.SeedWork;
+
+namespace AssetTracking.Domain.AggregatesModel.AssetAggregate
+{
+	public interface IAssetRepository : IRepository<Asset>
+	{
+		Task<bool> Insert(Asset asset);
+	}
+}
