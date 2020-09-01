eb48f55854c82a7e8175e5ce62c9c68f02d40858
Majid Parsa <MajidKahaki@gmail.com>
2020-09-01 20:42:42 +0430

    Add getCurrencies method

---
+++ AssetTracking.API/Controllers/CurrencyController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetTracking.API.Controllers
+{
+	
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CurrencyController : ControllerBase
+	{
+		public CurrencyController()
+		{
+		}
+
+		[HttpGet("getCurrencies")]
+		public ActionResult<IEnumerable<Currency>> Get()
+		{
+			var currencies = new List<Currency>()
+			{
+				new Currency("Rial", "Iran", 100, DateTime.Now),
+				new Currency("Dollar", "USA", 1000, DateTime.Now)
+			};
+
+			return Ok(currencies);
+		}
+	}
+
+	public class Currency
+	{
+		public Currency(string name, string country, int rate, DateTime date)
+		{
+			Name = name;
+			Country = country;
+			Rate = rate;
+			Date = date;
+		}
+
+		public string Name { get; set; }
+		public string Country { get; set; }
+		public int Rate { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
--- AssetTracking.API/Startup.cs
+++ AssetTracking.API/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AssetTracking
 {
@@ -24,12 +26,43 @@ namespace AssetTracking
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy",
+					builder => builder
+						.SetIsOriginAllowed((host) => true)
+						.AllowAnyMethod()
+						.AllowAnyHeader()
+						.AllowCredentials());
+			});
+
 			services.AddControllers();
+			services.AddSwaggerGen(c =>
+				c.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Version = "v1",
+					Title = "Asset Tracking API",
+					Description = "A simple example ASP.NET Core Web API",
+					//TermsOfService = new Uri("https://example.com/terms"),
+					//Contact = new OpenApiContact
+					//{
+					//	Name = "Shayne Boyer",
+					//	Email = string.Empty,
+					//	Url = new Uri("https://twitter.com/spboyer"),
+					//},
+					//License = new OpenApiLicense
+					//{
+					//	Name = "Use under LICX",
+					//	Url = new Uri("https://example.com/license"),
+					//}
+				}));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseCors("CorsPolicy");
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -43,6 +76,11 @@ namespace AssetTracking
 			{
 				endpoints.MapControllers();
 			});
+
+			app.UseSwagger();
+			app.UseSwaggerUI(
+				c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Asset Tracking API V1")
+			);
 		}
 	}
 }
