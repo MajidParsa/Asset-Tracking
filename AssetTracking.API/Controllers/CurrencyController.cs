27d1ee4806ff30e772f38c7d80afc8d05a4fb63a
Majid Parsa <MajidKahaki@gmail.com>
2020-09-12 14:26:03 +0430

    Add getCurrency method

--- AssetTracking.API/Controllers/CurrencyController.cs
+++ AssetTracking.API/Controllers/CurrencyController.cs
@@ -7,38 +7,67 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AssetTracking.API.Controllers
 {
-	
+
 	[Route("api/[controller]")]
 	[ApiController]
 	public class CurrencyController : ControllerBase
 	{
+		private IEnumerable<Currency> _currencies =>
+			new List<Currency>()
+			{
+				new Currency(1, "Rial", "Iran", 100, DateTime.Now),
+				new Currency(2, "Dollar", "USA", 1000, DateTime.Now)
+			};
+
+
 		public CurrencyController()
 		{
 		}
 
 		[HttpGet("getCurrencies")]
-		public ActionResult<IEnumerable<Currency>> Get()
+		public ActionResult<IEnumerable<Currency>> GetCurrencies()
 		{
-			var currencies = new List<Currency>()
-			{
-				new Currency("Rial", "Iran", 100, DateTime.Now),
-				new Currency("Dollar", "USA", 1000, DateTime.Now)
-			};
+			return Ok(_currencies);
+		}
 
-			return Ok(currencies);
+		[HttpGet("getCurrency")]
+		public ActionResult<Currency> GetCurrency(int id)
+		{
+			var currency = _currencies.FirstOrDefault(c => c.Id == id);
+			return Ok(currency);
+		}
+
+		[HttpPost("Add")]
+		public ActionResult<Currency> AddCurrency([FromBody] Currency currency)
+		{
+			currency.Rate = currency.Rate * 2;
+			return Ok(currency);
+		}
+
+		[HttpPost("Update")]
+		public ActionResult<Currency> UpdateCurrency([FromBody] Currency currency)
+		{
+			currency.Name = "Test";
+			return Ok(currency);
 		}
 	}
 
 	public class Currency
 	{
-		public Currency(string name, string country, int rate, DateTime date)
+		public Currency(int id, string name, string country, int rate, DateTime date)
 		{
+			Id = id;
 			Name = name;
 			Country = country;
 			Rate = rate;
 			Date = date;
 		}
 
+		public Currency()
+		{
+		}
+
+		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Country { get; set; }
 		public int Rate { get; set; }
